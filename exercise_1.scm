f3e44eb769b22e2664d8d1f86d9c668fa7d51b06
John Cowie <john.a.cowie@gmail.com>
2013-11-06 14:03:23 +0000

    Did some more work on exercise 1

--- exercise_1.scm
+++ exercise_1.scm
@@ -1,3 +1,5 @@
+; EXERCISE 1.1
+
 10 
 ;10
 
@@ -29,4 +31,42 @@
     b
     a)
 ;4
-    
+   
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+;16
+
+(+ 2 (if (> b a) b a))
+;6 
+
+(* (cond ((> a b) a)
+	 ((< a b) b)
+         (else -1))
+   (+ a 1))
+;16
+ 
+
+; EXERCISE 1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 1 3))))) (* 3 (- 6 2) (- 2 7)))
+
+; EXERCISE 1.3
+(define (sq-2-largest a b c) 
+  (cond ((and (< a b) (< a c)) (+ (* b b) (* c c)))
+        ((and (< b a) (< b c)) (+ (* a a) (* c c)))
+        (else (+ (* a a) (* b b)))))
+
+; EXERCISE 1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+ ; if b is positive add a to b, if b is negative subtract b from a
+
+; EXERCISE 1.5
+(define (p) (p))
+
+(define (test x y) 
+  (if (= x 0) 
+       0
+       y))
+
+
